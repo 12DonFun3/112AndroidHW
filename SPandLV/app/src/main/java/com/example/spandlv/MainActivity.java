41aa38ccc07696ae62889e19d7b166d4b1969f4f
12DonFun3 <a111221038@mail.shu.edu.tw>
2024-05-22 18:00:00 +0800

    2

--- SPandLV/app/src/main/java/com/example/spandlv/MainActivity.java
+++ SPandLV/app/src/main/java/com/example/spandlv/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.spandlv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         spinnerMealType = findViewById(R.id.spinnerMealType);
         listViewItems = findViewById(R.id.listViewItems);
-        tvSelection = findViewById(R.id.tvSelection);
+        tvSelection = findViewById(R.id.txvSelection);
 
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, mealTypes);
@@ -98,4 +104,31 @@ public class MainActivity extends AppCompatActivity {
                 + "Drink: " + selectedDrink;
         tvSelection.setText(selection);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.adjustmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.Send) {
+            // 点击“送出”按钮时跳转到 ConfirmationActivity
+            Intent intent = new Intent(MainActivity.this, ConfirmationActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (itemId == R.id.Cancel) {
+            // 点击“取消”按钮时清除所选项
+            selectedMainCourse = "";
+            selectedSideDish = "";
+            selectedDrink = "";
+            updateSelection();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }

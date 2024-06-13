e2051a33ea04db47bc0b6cf2d9cf867ae15218b5
12DonFun3 <a111221038@mail.shu.edu.tw>
2024-06-13 16:42:44 +0800

    0613

--- HW1_login/app/src/main/java/com/example/hw1_login/MainActivity.java
+++ HW1_login/app/src/main/java/com/example/hw1_login/MainActivity.java
@@ -4,17 +4,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewError;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 找到登录按钮
+        // 找到登录按钮和错误信息显示的TextView
         Button buttonLogin = findViewById(R.id.buttonLogin);
+        textViewError = findViewById(R.id.textViewError);
+
         // 设置按钮的点击事件监听器
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +34,11 @@ public class MainActivity extends AppCompatActivity {
                 if (username.equals("12345") && password.equals("54321")) {
                     // 登录成功，显示提示消息
                     Toast.makeText(MainActivity.this, "登入成功", Toast.LENGTH_SHORT).show();
+                    textViewError.setText("登入成功");
+                    //textViewError.setText(""); // 清空错误信息
                 } else {
                     // 登录失败，显示错误消息
+                    textViewError.setText("輸入錯誤: 帳號 - " + username + ", 密碼 - " + password);
                     Toast.makeText(MainActivity.this, "帳號或密碼錯誤", Toast.LENGTH_SHORT).show();
                 }
             }
